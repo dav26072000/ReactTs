334560fcf5eb129902e0ac51e07a0d8e7d827f91
dav26072000 <dav260720000@mail.ru>
2021-11-01 05:15:37 +0300

    feat(all doc): img after words

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
+import {Component} from 'react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { timeStamp } from 'console';
+import { TextTask } from "./components/TextTask";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  name: string;
 }
 
-export default App;
+interface AppProps {
+  melo?: string;
+}
+export class App extends Component<AppProps, AppState> {
+  render() {
+    return (
+      <div>
+        <TextTask/>
+      </div>
+    )
+  }
+}
+
+
